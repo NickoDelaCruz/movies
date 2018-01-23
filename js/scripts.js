11ca4e59165c700de75fbe1072a779ce28f2e1e5
Cameron Anderson and Nicko Dela Cruz <cameron@>
2018-01-23 10:42:30 -0800

    add another movie and well

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,16 @@
-function Movie(title, times, price) {
+function Movie(title, times, price, bg) {
   this.title = title;
   this.times = times;
   this.price = price;
-
-
-
-
-
+  this.bg = bg
 }
 
 function getMovies() {
   var movies = Array(0);
-  movies.push(new Movie("Finding Nemo", ['3:00pm', '7:00pm', '9:00pm'],10));
-  movies.push(new Movie("School of Rock", ['4:00pm', '8:00', '10:00PM'],10));
-  movies.push(new Movie("Twilight", ['5:00pm', '9:00', '11:00'],10));
+  movies.push(new Movie("Finding Nemo", ['3:00pm', '7:00pm', '9:00pm'],10, "nemo.jpg"));
+  movies.push(new Movie("School of Rock", ['4:00pm', '8:00', '10:00PM'],10, "school-of-rock.jpg"));
+  movies.push(new Movie("Twilight", ['5:00pm', '9:00', '11:00'],10, "twilight.jpg"));
+  movies.push(new Movie("Monster House", ["3:00PM", "5:00PM", "8:00PM"],10,"monsterhouse.jpg"));
   return movies;
 }
 
@@ -35,6 +32,7 @@ $(document).ready(function() {
 
     var index = parseInt($("#list").val());
     var times = movies[index].times;
+    $(".times").text("");
     times.forEach(function(time) {
       $(".times").append("<li>" + time + "</li>")
     });
@@ -42,7 +40,7 @@ $(document).ready(function() {
     var price = movies[index].price;
     if (age === "under13") price -= 2;
     else if (age === "senior") price -= 4;
-
-    $("#priceAge").append("<p>" + price + "</p>");
+    $("#priceAge").append("<p>$" + price + "</p>");
+    $('body').css('background-image', 'url(img/' + movies[index].bg + ')');
   });
 });
